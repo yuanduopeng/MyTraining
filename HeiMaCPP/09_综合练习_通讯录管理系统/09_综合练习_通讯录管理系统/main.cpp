301bb6066c1384c9be3957fd7f6ac0ee2fcb7757
yuanduopeng <yuanduopeng@qq.com>
2021-12-18 22:16:13 +0800

    211218 finish contactor exercise

--- HeiMaCPP/09_综合练习_通讯录管理系统/09_综合练习_通讯录管理系统/main.cpp
+++ HeiMaCPP/09_综合练习_通讯录管理系统/09_综合练习_通讯录管理系统/main.cpp
@@ -11,6 +11,7 @@
 
 #include <iostream>
 #include <string>
+#include <curses.h>
 #define MAX 1000
 using namespace std;
 
@@ -44,52 +45,180 @@ void printMenu()
 
 void printContactor(contactor cntc)
 {
-    cout << "联系人姓名：" << cntc.name << endl;
-    cout << "联系人性别：" << cntc.sex << endl;
-    cout << "联系人年龄：" << cntc.age << endl;
-    cout << "联系人电话：" << cntc.tel << endl;
-    cout << "联系人地址：" << cntc.adr << endl;
+    cout << "姓名：" << cntc.name << "\t";
+    cout << "性别：" << cntc.sex << "\t";
+    cout << "年龄：" << cntc.age << "\t";
+    cout << "电话：" << cntc.tel << "\t";
+    cout << "地址：" << cntc.adr << endl;
 }
 
-void addContactor(contactor cntc[], int * len)
+void addContactor(contactorBook * cntcB)
 {
-    int i = 0;
-    while(!cntc[i].name.empty())
+    int len = cntcB->m_size;
+    if (len == MAX)
     {
-        ++i;
+        cout << "通讯录已满，无法添加" << endl;
+        return;
     }
-    cout << "请输入联系人姓名" << endl;
-    cin >> cntc[i].name;
-    cout << "请输入联系人性别(1 or 2)：1.男 2.女" << endl;
-    int sex_int = 0;
-    bool sex_fault = 1;
-    while (sex_fault == 1)
+    else
     {
-        cin >> sex_int;
-        if (sex_int == 1)
+        cout << "请输入联系人姓名" << endl;
+        cin >> cntcB->cntc[len].name;
+        cout << "请输入联系人性别(1 or 2)：1.男 2.女" << endl;
+        int sex_int = 0;
+        bool sex_fault = 1;
+        while (sex_fault == 1)
         {
-            cntc[i].sex = "男";
-            sex_fault = 0;
+            cin >> sex_int;
+            if (sex_int == 1)
+            {
+                cntcB->cntc[len].sex = "男";
+                sex_fault = 0;
+            }
+            else if (sex_int == 2)
+            {
+                cntcB->cntc[len].sex = "女";
+                sex_fault = 0;
+            }
+            else
+            {
+                cout << "性别输入有误，请重新输入" << endl;
+                sex_fault = 1;
+            }
         }
-        else if (sex_int == 2)
+        cout << "请输入联系人年龄(0~150)" << endl;
+        cin >> cntcB->cntc[len].age;
+        cout << "请输入联系人电话" << endl;
+        cin >> cntcB->cntc[len].tel;
+        cout << "请输入联系人地址" << endl;
+        cin >> cntcB->cntc[len].adr;
+        cout << "录入成功，您输入的联系人信息为:" << endl;
+        printContactor(cntcB->cntc[len]);
+        ++cntcB->m_size;
+    }
+}
+
+void showContactor (contactorBook * cntcB)
+{
+    if (cntcB->m_size == 0)
+    {
+        cout << "当前无联系人" << endl;
+    }
+    else
+    {
+        for (int i=0; i<cntcB->m_size; i++)
         {
-            cntc[i].sex = "女";
-            sex_fault = 0;
-        }
-        else
-        {
-            cout << "性别输入有误，请重新输入" << endl;
-            sex_fault = 1;
+            printContactor(cntcB->cntc[i]);
         }
     }
-    cout << "请输入联系人年龄(0~150)" << endl;
-    cin >> cntc[i].age;
-    cout << "请输入联系人电话" << endl;
-    cin >> cntc[i].tel;
-    cout << "请输入联系人地址" << endl;
-    cin >> cntc[i].adr;
-    cout << "您输入的联系人信息为:" << endl;
-    printContactor(cntc[i]);
+}
+
+int isNameExist(contactorBook * cntcB, string n)
+{
+    for (int i=0; i<cntcB->m_size; i++)
+    {
+        if (cntcB->cntc[i].name == n)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void deleteContactor(contactorBook * cntcB)
+{
+    cout << "请输入需要删除的联系人姓名" << endl;
+    string nameToDel;
+    cin >> nameToDel;
+    int iToDel = isNameExist(cntcB, nameToDel);
+    if (iToDel == -1)
+    {
+        cout << "查无此人" << endl;
+    }
+    else
+    {
+        for (int i = iToDel; i < cntcB->m_size; ++i)
+        {
+            cntcB->cntc[i] = cntcB->cntc[i+1];
+        }
+        --cntcB->m_size;
+        cout << "删除成功" << endl;
+    }
+}
+
+void findContactor(contactorBook * cntcB)
+{
+    cout << "请输入需要查找的联系人姓名" << endl;
+    string nameToFind;
+    cin >> nameToFind;
+    int iToFind = isNameExist(cntcB, nameToFind);
+    if (iToFind == -1)
+    {
+        cout << "查无此人" << endl;
+    }
+    else
+    {
+        printContactor(cntcB->cntc[iToFind]);
+    }
+}
+
+void editContactor(contactorBook * cntcB)
+{
+    cout << "请输入需要编辑的联系人姓名" << endl;
+    string nameToEdit;
+    cin >> nameToEdit;
+    int iToEdit = isNameExist(cntcB, nameToEdit);
+    if (iToEdit == -1)
+    {
+        cout << "查无此人" << endl;
+    }
+    else
+    {
+        cout << "请重新输入联系人姓名" << endl;
+        cin >> cntcB->cntc[iToEdit].name;
+        cout << "请重新输入联系人性别(1 or 2)：1.男 2.女" << endl;
+        int sex_int = 0;
+        bool sex_fault = 1;
+        while (sex_fault == 1)
+        {
+            cin >> sex_int;
+            if (sex_int == 1)
+            {
+                cntcB->cntc[iToEdit].sex = "男";
+                sex_fault = 0;
+            }
+            else if (sex_int == 2)
+            {
+                cntcB->cntc[iToEdit].sex = "女";
+                sex_fault = 0;
+            }
+            else
+            {
+                cout << "性别输入有误，请重新输入" << endl;
+                sex_fault = 1;
+            }
+        }
+        cout << "请重新输入联系人年龄(0~150)" << endl;
+        cin >> cntcB->cntc[iToEdit].age;
+        cout << "请重新输入联系人电话" << endl;
+        cin >> cntcB->cntc[iToEdit].tel;
+        cout << "请重新输入联系人地址" << endl;
+        cin >> cntcB->cntc[iToEdit].adr;
+        cout << "修改成功，您重新输入的联系人信息为:" << endl;
+        printContactor(cntcB->cntc[iToEdit]);
+    }
+}
+
+void cleanContactor(contactorBook * cntcB)
+{
+    cout << "确定要清空联系人吗？1.确认 2.放弃" << endl;
+    bool sure = 0;
+    cin >> sure;
+    if (sure)
+    {
+        cntcB->m_size = 0;
+    }
+    cout << "清空完成" << endl;
 }
 
 int main(int argc, const char * argv[]) {
@@ -103,17 +232,22 @@ int main(int argc, const char * argv[]) {
         switch (userInput)
         {
             case 1: cout << "请添加联系人信息" << endl;
-                addContactor(cB.cntc, &cB.m_size);
+                addContactor(&cB);
                 break;
-            case 2: cout << "请输入需要显示的联系人姓名" << endl;
+            case 2:
+                showContactor(&cB);
                 break;
-            case 3: cout << "请输入需要删除的联系人姓名" << endl;
+            case 3:
+                deleteContactor(&cB);
                 break;
-            case 4: cout << "请输入需要查找的联系人信息" << endl;
+            case 4:
+                findContactor(&cB);
                 break;
-            case 5: cout << "请输入需要修改的联系人姓名" << endl;
+            case 5:
+                editContactor(&cB);
                 break;
-            case 6: cout << "确定要清空联系人吗？" << endl;
+            case 6:
+                cleanContactor(&cB);
                 break;
             case 0:
                 break;
